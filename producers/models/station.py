31773b2298092946095a634668ee33b06ae884ed
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-12-17 12:32:44 +0000

    mod prod: station. try except gone

--- producers/models/station.py
+++ producers/models/station.py
@@ -54,24 +54,24 @@ class Station(Producer):
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
         # TODO: Complete this function by producing an arrival message to Kafka, done
-
-        try:
-            logger.info(f"{self.topic_name}: train {train.train_id} arrived from direction {direction} and prev_direction {prev_direction} and prev_station_id {prev_station_id} ")
-            self.producer.produce(
-                topic=self.topic_name,
-                key={"timestamp": self.time_millis()},
-                value={"station_id":self.station_id,
-                  "train_id":train.train_id,
-                  "direction":direction,
-                  "line":self.color.name,
-                  "train_status":train.status.name,
-                  "prev_station_id":prev_station_id,
-                  "prev_direction":prev_direction,
-           },
-                value_schema=Station.value_schema,
-            )
-        except:
-            logger.info("arrival kafka integration incomplete - skipping")                
+       
+    logger.info(f"{self.topic_name}: train {train.train_id} arrived from direction \
+        {direction} and prev_direction {prev_direction} \
+        and prev_station_id {prev_station_id} ")
+    
+    self.producer.produce(
+        topic=self.topic_name,
+        key={"timestamp": self.time_millis()},
+        value={"station_id":self.station_id,
+               "train_id":train.train_id,
+               "direction":direction,
+               "line":self.color.name,
+               "train_status":train.status.name,
+               "prev_station_id":prev_station_id,
+               "prev_direction":prev_direction,
+              },
+        value_schema=Station.value_schema,
+    )
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
