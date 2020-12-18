61afe9341a3e8dce644fe73d3b8f4d8144de499a
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-12-18 20:37:48 +0000

    cons faust: first working draft

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -33,26 +33,44 @@ class TransformedStation(faust.Record):
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("SQL_stations", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("SQL_stations_transformed", partitions=1)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+transtable = app.Table(
+    "transformed_stations_table",
+    default=int,
+    partitions=1,
+    changelog_topic=out_topic,
+)
 
-
-#
-#
 # TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
 # "line" is the color of the station. So if the `Station` record has the field `red` set to true,
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
 
+@app.agent(topic)
+async def transform_station(stations):
+    async for station in stations:
+        if station.red == True:
+            line = "red"
+        if station.green == True:
+            line = "green"
+        if station.blue == True:
+            line = "blue"
+        else:
+            line = None
+        #print(line)
+        
+        ts = TransformedStation(
+                station.station_id, 
+                station.station_name, 
+                station.order,
+                line)
+        transtable[station.station_name] = ts
+        print({"station_id": station.station_id, 
+              "station_name": station.station_name, 
+              "statio_order": station.order,
+              "line": line})
 
 if __name__ == "__main__":
     app.main()
