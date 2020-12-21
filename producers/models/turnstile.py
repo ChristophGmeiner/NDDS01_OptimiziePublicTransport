fac6eaa70a43c9344ef51880106f72b25867f37f
ChristophGmeiner <christoph.gmeiner@gmail.com>
2020-12-21 12:46:01 +0000

    prod turnstile.py: one topic for all turnstiles

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -34,7 +34,7 @@ class Turnstile(Producer):
         # replicas
         
         super().__init__(
-            f"turnstiles_topic_{station_name}", # TODO: Come up with a better topic name, done
+            "turnstiles_topic", # TODO: Come up with a better topic name, done
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=1,
